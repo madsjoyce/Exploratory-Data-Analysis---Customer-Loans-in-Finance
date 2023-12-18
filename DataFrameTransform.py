54e4e28d112f9ad8bb33055935a7ac9aa9cb7c3f
madsjoyce <madeleinevelyn@outlook.com>
2023-12-18 15:14:23 +0000

    Milestone 3 Changes

--- DataFrameTransform.py
+++ DataFrameTransform.py
@@ -1,20 +1,117 @@
-#import pandas as pd 
+import numpy as np
 
 class DataFrameTransform:
     
-    def drop_columns(df, threshold=30):
+    def null_percentage(df):
+        # Calculate the percentage of missing values in each column
+        null_percentage = (df.isnull().sum() / len(df)) * 100
+        return null_percentage
+    
+    def drop_columns(df, threshold=50):
         # Drop columns with missing values exceeding the threshold
         null_percentage = DataFrameTransform.null_percentage(df)
         columns_to_drop = null_percentage[null_percentage > threshold].index
         df = df.drop(columns=columns_to_drop)
         return df
 
-    def impute_nulls(df, strategy='median'):
-        # Impute missing values using median or mean
-        if strategy == 'median':
-            df = df.fillna(df.median())
-        elif strategy == 'mean':
-            df = df.fillna(df.mean())
+    def impute_mean(df, columns=None):
+    # If specific columns are provided, only impute those columns
+        if columns is not None:
+            if not set(columns).issubset(df.columns):
+                raise ValueError("Invalid column names provided.")
+
+            df[columns] = df[columns].fillna(df[columns].mean())
         else:
-            raise ValueError("Invalid imputation strategy. Choose 'median' or 'mean'.")
+            pass
+
+        return df
+
+    def impute_median(df, columns=None):
+        if columns is not None:
+            if not set(columns).issubset(df.columns):
+                raise ValueError("Invalid column names provided.")
+
+            df[columns] = df[columns].fillna(df[columns].median())
+        else:
+            pass
+
+        return df
+    
+    def impute_mode(df, columns=None):
+        if columns is not None:
+            if not set(columns).issubset(df.columns):
+                raise ValueError("Invalid column names provided.")
+
+            for column in columns:
+                mode_value = df[column].mode().iloc[0]
+                df[column] = df[column].fillna(mode_value)
+        else:
+            pass
+
+        return df
+    
+    def remove_outliers(df, columns):
+        """
+        Remove outliers from specified columns in the DataFrame.
+
+        Parameters:
+        - df: pandas.DataFrame
+        - columns: list of str, columns to remove outliers from
+
+        Returns:
+        pandas.DataFrame
+        """
+        for column in columns:
+            # Calculate IQR (Interquartile Range)
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+
+            # Define the outlier bounds
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+
+            # Remove outliers
+            df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+        return df
+
+    def identify_highly_correlated_columns(df, threshold=0.8):
+        """
+        Identify highly correlated columns in the DataFrame.
+
+        Parameters:
+        - df: pandas.DataFrame
+        - threshold: float, correlation threshold to consider
+
+        Returns:
+        List of tuples containing highly correlated column pairs.
+        """
+        numerical_columns = df.select_dtypes(include=['number'])
+        if not numerical_columns.empty:
+            mask = np.triu(np.ones_like(numerical_columns.corr(), dtype=bool), k=1)
+            correlated_pairs = [
+                (col1, col2) 
+                for col1 in numerical_columns.columns 
+                for col2 in numerical_columns.columns 
+                if mask[numerical_columns.columns.get_loc(col1), numerical_columns.columns.get_loc(col2)] and abs(numerical_columns[col1].corr(numerical_columns[col2])) > threshold
+                ]
+
+            return correlated_pairs
+        else:
+            print("No numerical columns to identify correlations.")
+            return []
+
+    def remove_highly_correlated_columns(df, highly_correlated_columns):
+        """
+        Remove highly correlated columns from the DataFrame.
+
+        Parameters:
+        - df: pandas.DataFrame
+        - correlated_columns: highly correlated column pairs
+        """
+        for col1, col2 in highly_correlated_columns:
+            # Decide which column to keep (e.g., based on domain knowledge or preference)
+            # Here, we choose to keep col1
+            df = df.drop(col2, axis=1, errors='ignore')
+
         return df
--- Plotter.py
+++ Plotter.py
@@ -1,29 +1,158 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
+
 
 class Plotter:
 
-   def null_percentage(df):
-        # Calculate the percentage of missing values in each column
+     def null_percentage(df):
+        '''
+        Works out percentage of null values
+
+        Parameters:
+        - df: pandas.DataFrame
+
+        Returns:
+        null_percentage
+        '''
         null_percentage = (df.isnull().sum() / len(df)) * 100
         return null_percentage
 
-   def plot_nulls(df):
+     def plot_nulls(df):
+        '''
+        Plots null values on graph
+
+        Paramaters:
+        - df: pandas.DataFrame
+
+        Returns:
+        null_columns
+        '''
         plt.figure(figsize=(10, 6))
         sns.heatmap(df.isnull(), cbar=False, cmap='viridis', yticklabels=False)
         plt.title("Missing Values in DataFrame")
         plt.show()
 
-   def columns_with_nulls(df):
+     def columns_with_nulls(df):
+       '''
+       Finds column names with null values present
+
+        Paramaters:
+        - df: pandas.DataFrame
+
+        Returns:
+        null_columns
+       '''
        null_columns = df.columns[df.isnull().any()]
        return null_columns
 
 
-   def nulls_above_50_percent(df):
+     def nulls_above_50_percent(df):
+       '''
+       Finds column names with null value presence over 50%
+
+        Paramaters:
+        - df: pandas.DataFrame   
+
+        Returns:
+        null_columns
+       '''
        null_percentage = (df.isnull().sum() / len(df)) * 100
        columns_above_threshold = null_percentage[null_percentage > 50].index
        if not columns_above_threshold.empty:
          print(f"Columns with more than 50% null values: {columns_above_threshold}")
        else:
          print("No columns have more than 50% null values.")
+
+     def identify_skewed_columns(df, skew_threshold=0.5):
+       ''' 
+       Identify skewed columns.
+       
+       Parameters:
+       - df: pandas.DataFrame
+       - skew_threshold: threshold at which data becomes too skewed.
+       
+       Returns:
+       Skewed_columns 
+       '''
+       numeric_columns = df.select_dtypes(include=np.number).columns
+       skewed_columns = df[numeric_columns].apply(lambda x: np.abs(x.skew()) > skew_threshold)
+       return skewed_columns[skewed_columns].index.tolist()
+
+     def apply_best_transformation(df, skewed_columns):
+       for column in skewed_columns:
+           if pd.api.types.is_numeric_dtype(df[column]):
+               df[column] = np.log1p(df[column])
+       return df
+
+     def visualise_original_skewness(df, skewed_columns):
+          """
+          Visualise skewness of columns in the original DataFrame before transformations.
+
+          Parameters:
+           - df: original pandas.DataFrame
+             Original DataFrame before transformations.
+           - skewed_columns: list of str
+            Columns identified as skewed.
+           """
+          plt.figure(figsize=(6, 6))
+    
+          # Visualise skewness before transformations
+          sns.histplot(data=df[skewed_columns], kde=True)
+          plt.title("Skewness Before Transformations")
+    
+          plt.show()
+
+     def visualise_transformed_skewness(df, skewed_columns):
+          """
+          Visualise skewness of columns in the transformed DataFrame after transformations.
+
+          Parameters:
+          - df: transformed pandas.DataFrame
+           DataFrame after transformations.
+          - skewed_columns: list of str
+          Columns identified as skewed.
+          """
+          plt.figure(figsize=(6, 6))
+    
+          # Visualize skewness after transformations
+          sns.histplot(data=df[skewed_columns], kde=True)
+          plt.title("Skewness After Transformations")
+    
+          plt.show()
+
+     def visualise_boxplots(df):
+          """
+          Visualise boxplots for numerical columns in the DataFrame.
+
+          Parameters:
+          - df: pandas.DataFrame
+          """
+          numerical_columns = df.select_dtypes(include=['number']).columns
+
+          plt.figure(figsize=(14, 8))
+          df[numerical_columns].boxplot(sym='k.', notch=True, vert=False, patch_artist=True)
+        
+          plt.title("Boxplots for Numerical Columns")
+          plt.xlabel("Values")
+          plt.ylabel("Columns")
+          plt.show()
+      
+     def visualise_heatmap(correlation_matrix):
+        """
+        Visualise heatmap for correlation matrix.
+
+        Parameters:
+        - correlation_matrix: pandas.DataFrame, correlation matrix
+        """
+        numerical_columns = correlation_matrix.select_dtypes(include=['number']).columns
+        if not numerical_columns.empty:
+            correlation_matrix_numerical = correlation_matrix[numerical_columns]
+
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(correlation_matrix_numerical, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
+            plt.title("Correlation Matrix Heatmap (Numerical Data)")
+            plt.show()
+        else:
+            print("No numerical columns to visualize.")
